d998ff341b74c2091e05c38eca5131ea60877e6b
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-14 16:32:11 +0200

    validates pour mvp done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,17 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :galleries, dependent: :destroy
   has_many :registrations, dependent: :destroy
+  # J'ai mis un validate afin que les users n'aient pas le même username
+  # Je l'ai renforcé avec case_sensitive donc Benji et benji sont egaux
+  # J'ai également mis un message pour une UX plus propre
+  validates :user_name, presence: { message: "Vous devez renseigner un nom d'utilisateur" }, uniqueness:
+  { case_sensitive: false, message: "Ce nom d'utilisateur est déjà pris" }
+  validates :first_name, presence: { message: "Vous devez renseigner un prénom" }
+  validates :last_name, presence: { message: "Vous devez renseigner un nom" }
+  validates :email, presence: { message: "Vous devez renseigner un email" }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone_number, presence: { message: "Vous devez renseigner un numéro de téléphone" }, format:
+  { with: /\A0\d{9}\z/, message: "format invalide - 10 chiffres sans espace (ex: 0612345678)" }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
