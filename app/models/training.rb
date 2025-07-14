26fcfc14c3e0a63af15f82248decb7c73c5fa06c
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-14 15:15:00 +0200

    association table users done

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,4 @@
 class Training < ApplicationRecord
   belongs_to :user
+  has_many :registrations, as: :registerable, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :events, dependent: :destroy
+  has_many :trainings, dependent: :destroy
+  has_many :races, dependent: :destroy
+  has_many :articles, dependent: :destroy
+  has_many :galleries, dependent: :destroy
+  has_many :registrations, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
