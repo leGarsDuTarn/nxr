41b2c6addeacb87963bb106ee19170ec0706f22c
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-16 17:12:21 +0200

    test unitaire model gallery sur validation - done

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,7 +4,8 @@ class Gallery < ApplicationRecord
   # Offre à l'admin la possibilté d'ajouter des images pour la création d'une galerie
   has_one_attached :image
   # Validation obligatoire pour pouvoir créer une gallerie
-  validates :title, presence: { message: "Vous devez renseigner un titre" }
+  validates :title, presence: { message: "Vous devez renseigner un titre" }, uniqueness:
+  { message: "Ce titre est déjà utilisé" }
   validates :image, presence: { message: "Vous devez ajouter une image" }
   validates :date, presence: { message: "Vous devez renseigner une date" }
 end
