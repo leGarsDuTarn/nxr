aaf89c9fa5a1aef4e645512635d841e40b854b2c
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-18 16:21:30 +0200

    creation methode index, show, new

--- app/controllers/admin/trainings_controller.rb
+++ app/controllers/admin/trainings_controller.rb
@@ -1,4 +1,25 @@
 module Admin
   class TrainingsController < BaseController
+    before_action :set_admin_training, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @trainings = Training.all
+    end
+
+    def show
+      # @training est déjà défini par set_admin_training
+    end
+
+    def new
+      @training = Training.new
+    end
+
+    def create
+      @training = Training.new
+      @training.user = current_user
+        if @training.save
+          redirect_to 
+        end
+    end
   end
 end
