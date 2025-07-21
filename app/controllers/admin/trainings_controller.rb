a90e2ec0f2763a103699beace584c98276468146
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-21 09:24:13 +0200

    Codage du CRUD complet pour training controller cote admin

--- app/controllers/admin/trainings_controller.rb
+++ app/controllers/admin/trainings_controller.rb
@@ -7,7 +7,7 @@ module Admin
     end
 
     def show
-      # @training est déjà défini par set_admin_training
+      # @training - déjà défini par set_admin_training
     end
 
     def new
@@ -17,9 +17,38 @@ module Admin
     def create
       @training = Training.new
       @training.user = current_user
-        if @training.save
-          redirect_to 
-        end
+      if @training.save
+        redirect_to admin_training_path(@training), notice: "Entraînement crée avec succès"
+      else
+        render :new, status: :unprocessable_entity, alerte: "Erreur lors de la création de l'entraînement"
+      end
+    end
+
+    def edit
+      # @training - déjà défini par set_admin_training
+    end
+
+    def update
+      if @training.update(training_params)
+        redirect_to admin_training_path(@training), notice: "Modification réussie"
+      else
+        render :edit, status: :unprocessable_entity, alerte: "Erreur lors de la modification"
+      end
+    end
+
+    def destroy
+      @training.destroy
+      redirect_to admin_dashboard_path, status: :see_other
+    end
+
+    private
+
+    def set_admin_training
+      @training = Training.find(params[:id])
+    end
+
+    def training_params
+      params.require(:training).permit(:name, :description, :date, :hour, :image)
     end
   end
 end
