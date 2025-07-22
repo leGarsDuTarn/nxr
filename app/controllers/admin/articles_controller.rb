03ec13fecc012953141c60e358fdc37bb9f9c14c
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-22 11:28:48 +0200

    ecriture de la methode index du controller Admin::Article

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,4 +1,9 @@
 module Admin
   class ArticlesController < BaseController
+    before_action :set_admin_article, only: %i[show edit update destroy]
+
+    def index
+      @articles = Article.all
+    end
   end
 end
