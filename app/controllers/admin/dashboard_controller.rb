d2953808e1516cdb3625a8575890c2dab41c252c
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-22 16:35:44 +0200

    Realisation d'un front sommaire pour realisation test manuels - test ok - amelioration syntaxe sur alert: et before_action des controllers

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,7 @@ module Admin
       @events = Event.all.order(date: :asc).limit(2)
       @trainings = Training.all.order(date: :asc).limit(4)
       @races = Race.all.order(date: :asc).limit(5)
+      @articles = Article.all.order(date: :asc).limit(3)
     end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,9 @@ class Article < ApplicationRecord
 
   # Offre à l'admin la possibilité d'ajouter des images pour la création d'un article
   has_one_attached :image
+  # Permet de supprimer l'image via le questionnaire view/articles/_form.html.erb
+  attr_accessor :remove_image
+
   # Validation obligatoire pour pouvoir créer un article
   validates :title, presence: { message: "Vous devez renseigner un titre" }, uniqueness:
   { message: "Ce titre est déjà utilisé" }
