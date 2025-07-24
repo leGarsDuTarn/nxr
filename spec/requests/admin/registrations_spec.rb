1a3296d0a228cb8cf1f3fe373b7b95a564d2d573
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-24 14:21:25 +0200

    Ecriture des tests de type request pour le controller Admin::Registration - done

--- spec/requests/admin/registrations_spec.rb
+++ spec/requests/admin/registrations_spec.rb
@@ -70,8 +70,8 @@ RSpec.describe "Admin::Registrations", type: :request do
     training
   end
 
-  describe "GET/admin/events/:events_id/registrations" do # Méthode Index event
-    context "Quand un admin accéde à une inscription d'une activité de type event" do
+  describe "GET /admin/events/:event_id/registrations" do # Méthode Index event
+    context "Quand un admin accéde aux inscriptions d'une activité de type event" do
       it " retourne un status 200, affiche les inscriptions et valide le test" do
         get admin_event_registrations_path(event, format: :html)
         expect(response).to have_http_status(:ok)
@@ -80,8 +80,8 @@ RSpec.describe "Admin::Registrations", type: :request do
     end
   end
 
-  describe "GET/admin/races/:races_id/registrations" do # Méthode Index race
-    context "Quand un admin accéde à une inscription d'une activité de type race" do
+  describe "GET /admin/races/:race_id/registrations" do # Méthode Index race
+    context "Quand un admin accéde aux inscriptions d'une activité de type race" do
       it " retourne un status 200, affiche les inscriptions et valide le test" do
         get admin_race_registrations_path(race, format: :html)
         expect(response).to have_http_status(:ok)
@@ -90,8 +90,8 @@ RSpec.describe "Admin::Registrations", type: :request do
     end
   end
 
-  describe "GET/admin/trainings/:trainings_id/registrations" do # Méthode Index training
-    context "Quand un admin accéde à une inscription d'une activité de type training" do
+  describe "GET /admin/trainings/:training_id/registrations" do # Méthode Index training
+    context "Quand un admin accéde aux inscriptions d'une activité de type training" do
       it " retourne un status 200, affiche les inscriptions et valide le test" do
         get admin_training_registrations_path(training, format: :html)
         expect(response).to have_http_status(:ok)
@@ -100,12 +100,12 @@ RSpec.describe "Admin::Registrations", type: :request do
     end
   end
 
-  describe "GET/admin/events/:events_id/registrations" do # Méthode show event
+  describe "GET /admin/events/:event_id/registrations/:id" do # Méthode show event
     context "Quand un admin est connecté et affiche les détails des inscriptions à un événement de type event" do
       it "retourne un status 200, affiche les informations des users inscrit" do
-        # Vérifie que les informations du participant inscrit à l'événement sont bien affichées à l'admin
-        Registration.create!(user: user, registerable: event)
-        get admin_event_registrations_path(event)
+        # Crée une inscription qui sera visible pour l'admin
+        registration = Registration.create!(user: user, registerable: event)
+        get admin_event_registration_path(event, registration)
         expect(response).to have_http_status(:ok)
         expect(response.body).to include(user.last_name)
         expect(response.body).to include(user.first_name)
@@ -113,12 +113,12 @@ RSpec.describe "Admin::Registrations", type: :request do
     end
   end
 
-  describe "GET/admin/races/:races_id/registrations" do # Méthode show race
+  describe "GET /admin/races/:race_id/registrations/:id" do # Méthode show race
     context "Quand un admin est connecté et affiche les détails des inscriptions à un événement de type race" do
       it "retourne un status 200, affiche les informations des users inscrit" do
-        # Vérifie que les informations du participant inscrit à l'événement sont bien affichées à l'admin
-        Registration.create!(user: user, registerable: race)
-        get admin_race_registrations_path(race)
+        # Crée une inscription qui sera visible pour l'admin
+        registration = Registration.create!(user: user, registerable: race)
+        get admin_race_registration_path(race, registration)
         expect(response).to have_http_status(:ok)
         expect(response.body).to include(user.last_name)
         expect(response.body).to include(user.first_name)
@@ -126,12 +126,12 @@ RSpec.describe "Admin::Registrations", type: :request do
     end
   end
 
-  describe "GET/admin/trainings/:trainings_id/registrations" do # Méthode show training
+  describe "GET /admin/trainings/:training_id/registrations/:id" do # Méthode show training
     context "Quand un admin est connecté et affiche les détails des inscriptions à un événement de type training" do
       it "retourne un status 200, affiche les informations des users inscrit" do
-        # Vérifie que les informations du participant inscrit à l'événement sont bien affichées à l'admin
-        Registration.create!(user: user, registerable: training)
-        get admin_training_registrations_path(training)
+        # Crée une inscription qui sera visible pour l'admin
+        registration = Registration.create!(user: user, registerable: training)
+        get admin_training_registration_path(training, registration)
         expect(response).to have_http_status(:ok)
         expect(response.body).to include(user.last_name)
         expect(response.body).to include(user.first_name)
@@ -139,14 +139,42 @@ RSpec.describe "Admin::Registrations", type: :request do
     end
   end
 
-  describe "DELETE/admin/events/:events_id/registrations" do # Méthode destroy event
-    context "Quand l'admin delete une inscription lié à événement de type course" do
+  describe "DELETE /admin/events/:event_id/registrations/:id" do # Méthode destroy event
+    context "Quand l'admin delete une inscription lié à événement de type event" do
       it "supprime l'inscription, redirige l'user (302), et valide le test" do
-        # Vérifie que les informations du participant inscrit à l'événement sont bien affichées à l'admin
+        # Crée une inscription qui sera supprimée pour le test
         registration = Registration.create!(user: user, registerable: event)
         # expect {...} permet de tester un changement d'état, test les créations et suppressions
         expect {
           delete admin_event_registration_path(event, registration)
+        }.to change(Registration, :count).by(-1) # Ici permet de vérifier que l'inscription est bien supprimé en DB
+        expect(response).to have_http_status(:redirect) # Redirige l'user après suppression (302)
+      end
+    end
+  end
+
+  describe "DELETE /admin/races/:race_id/registrations/:id" do # Méthode destroy race
+    context "Quand l'admin delete une inscription lié à événement de type race" do
+      it "supprime l'inscription, redirige l'user (302), et valide le test" do
+        # Crée une inscription qui sera supprimée pour le test
+        registration = Registration.create!(user: user, registerable: race)
+        # expect {...} permet de tester un changement d'état, test les créations et suppressions
+        expect {
+          delete admin_race_registration_path(race, registration)
+        }.to change(Registration, :count).by(-1) # Ici permet de vérifier que l'inscription est bien supprimé en DB
+        expect(response).to have_http_status(:redirect) # Redirige l'user après suppression (302)
+      end
+    end
+  end
+
+  describe "DELETE /admin/trainings/:training_id/registrations/:id" do # Méthode destroy training
+    context "Quand l'admin delete une inscription lié à événement de type training" do
+      it "supprime l'inscription, redirige l'user (302), et valide le test" do
+        # Crée une inscription qui sera supprimée pour le test
+        registration = Registration.create!(user: user, registerable: training)
+        # expect {...} permet de tester un changement d'état, test les créations et suppressions
+        expect {
+          delete admin_training_registration_path(training, registration)
         }.to change(Registration, :count).by(-1) # Ici permet de vérifier que l'inscription est bien supprimé en DB
         expect(response).to have_http_status(:redirect) # Redirige l'user après suppression (302)
       end
