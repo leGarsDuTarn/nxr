e31f8f8fa0375065bb45357a80efbf316c3fc657
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-24 09:53:57 +0200

    migration pour rajouter a la table Registrations un index unique pour eviter les doublons d'inscription

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -5,11 +5,16 @@ class Registration < ApplicationRecord
   # Permets d'éviter les doublons d'inscription
   validates :user_id, uniqueness: { scope: %i[registerable_type registerable_id], message: "Vous êtes déjà inscrit" }
   # Permets de refuser l'inscription d'un utilisateur à un events, training ou race si ces activités sont terminées
-  # Ici validate custom
+  # Ici validate custom - empêche l'inscription si l'activité est déjà passée
   validate :activity_open
+  # Permets ici de limiter l'inscription uniquement aux trois activités lister ci-dessous [Event, Race, Training]
+  validates :registerable_type, inclusion: {
+    in: %w[Event Race Training],
+    message: "%<value> n’est pas un type d’inscription valide"
+  }
 
   def activity_open
-    return if registerable.respond_to(:date) && registerable.date < Date.today
+    return if registerable.respond_to(:date) && registerable.date >= Date.today
 
     errors.add(:registerable, "cet événement n'est plus ouvert aux inscriptions")
   end
