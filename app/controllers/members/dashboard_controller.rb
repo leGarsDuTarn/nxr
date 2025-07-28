6378f8dee9330af9d7b7f83ff218c8e990d78749
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-28 21:21:06 +0200

    Creation d'un front sommaire pour le dashboard membre afin de pouvoir realiser les test manuel d'inscription

--- app/controllers/members/dashboard_controller.rb
+++ app/controllers/members/dashboard_controller.rb
@@ -1,6 +1,7 @@
 module Members
   class DashboardController < BaseController
     def index
+      @registrations = current_user.registrations.includes(:registerable)
       @races = Race.all
     end
   end
