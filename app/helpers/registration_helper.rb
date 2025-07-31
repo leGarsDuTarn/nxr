34f2cd5783c937f4f5cf679ddff892767a263a71
Benjamin Grassiano <grassiano.b@gmail.com>
2025-07-31 09:51:27 +0200

    Amelioration UX sur une show de race pour que le membre ne puisse pas cliquer sur le bouton s'inscrire

--- app/helpers/registration_helper.rb
+++ app/helpers/registration_helper.rb
@@ -6,14 +6,21 @@ module RegistrationHelper
     # Guard Clause -> vérifie si l'utilisateur connecté est un membre ou un admin si false ne rentre pas dans la méthode
     return unless current_user&.member? || current_user&.admin?
 
-    path = case resource
-           when Race
-             new_members_race_registration_path(resource)
-           when Training
-             new_members_training_registration_path(resource)
-           when Event
-             new_members_event_registration_path(resource)
-           end
-    link_to "Je m'inscris", path, class: "btn btn-success" if path
+    # Permet de vérifier si l'utilisateur est déjà inscrit à cette ressource
+    already_registered = current_user.registrations.exists?(registerable: resource)
+
+    if already_registered
+      content_tag(:span, "Vous êtes déja inscrit", class: "btn btn-secondary disabled")
+    else
+      path = case resource
+             when Race
+               new_members_race_registration_path(resource)
+             when Training
+               new_members_training_registration_path(resource)
+             when Event
+               new_members_event_registration_path(resource)
+             end
+      link_to "Je m'inscris", path, class: "btn btn-success" if path
+    end
   end
 end
