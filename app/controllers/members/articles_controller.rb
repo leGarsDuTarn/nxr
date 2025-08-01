b9201f18ccc12d7f787ca2ba5635718ca8b490cc
Benjamin Grassiano <grassiano.b@gmail.com>
2025-08-01 14:58:16 +0200

    Creation des methodes index et show dans les controllers article et galleries

--- app/controllers/members/articles_controller.rb
+++ app/controllers/members/articles_controller.rb
@@ -1,5 +1,13 @@
 module Members
   class ArticlesController < BaseController
-    skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user! # Permet de rendre la page public sans création de compte
+
+    def index
+      @articles = Article.all.order(created_at: :desc) # :desc -> + récent en premier
+    end
+
+    def show
+      @article = Article.find(params[:id])
+    end
   end
 end
--- app/controllers/members/galleries_controller.rb
+++ app/controllers/members/galleries_controller.rb
@@ -1,5 +1,13 @@
 module Members
   class GalleriesController < BaseController
-    skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user! # Permet de rendre la page public sans création de compte
+
+    def index
+      @galleries = Gallery.all.order(created_at: :desc) # :desc -> + récent en premier
+    end
+
+    def show
+      @gallerie = Gallerie.find(params[:id])
+    end
   end
 end
