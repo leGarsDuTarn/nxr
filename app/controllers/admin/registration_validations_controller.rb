30487e228de9d4706567da3e49cf0d27fbb0f830
Benjamin Grassiano <grassiano.b@gmail.com>
2025-08-04 11:07:59 +0200

    Ajout d'un champ status au modele registration avec un enum, creation d'un controller registration_validations cote admin, creation des routes custom PATCH et ecriture des methodes validate et reject

---
+++ app/controllers/admin/registration_validations_controller.rb
@@ -0,0 +1,24 @@
+module Admin
+  class RegistrationValidationsController < BaseController
+    before_action :set_status_registration
+
+    def validate
+      # Change le statut de l'inscription en "validated" (grâce à l'enum dans le modèle qui est par default "pending")
+      @registration.validated!
+      # Redirige l'admin vers la page précédente (ex: la course), ou vers le dashboard si la redirection échoue
+      # Affiche un message flash de confirmation
+      redirect_back fallback_location: admin_dashboard_path, notice: "Inscription validée avec succès."
+    end
+
+    def reject
+      @registration.rejected!
+      redirect_back fallback_location: admin_dashboard_path, alert: "Inscription refusée"
+    end
+
+    private
+
+    def set_status_registration
+      @registration = Registration.find(params[:id])
+    end
+  end
+end
