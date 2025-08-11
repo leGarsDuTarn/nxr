1341b23dafc1be081f7bb3cf056ad426ac47e394
benjamin grassiano <grassiano.b@gmail.com>
2025-08-11 13:02:36 +0200

    Creation de la methode home pour affichages des evenements type course ou training

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # Permet d'afficher uniquement les courses à venir
+    @races = Race.where("date >= ?", Date.today).order(date: :asc)
   end
 end
