0b259ef90aaf82148717d2e57cc059b9150ced14
benjamin grassiano <grassiano.b@gmail.com>
2025-08-12 16:06:55 +0200

    Creation des cards pour les evenements a venir + correction du style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,23 @@ class PagesController < ApplicationController
 
   def home
     # Permet d'afficher les événements à venir
-    @races = Race.where("date >= ?", Date.today).order(date: :asc).limit(2)
-    @trainings = Training.where("date >= ?", Date.today).order(date: :asc).limit(2)
-    @events = Event.where("date >= ?", Date.today).order(date: :asc).limit(2)
+   races = Race.where("date >= ?", Date.today)
+              .order(:date)
+              .limit(3)
+              .select("id, name, date, 'Race' as type")
+
+  events = Event.where("date >= ?", Date.today)
+                .order(:date)
+                .limit(3)
+                .select("id, name, date, 'Event' as type")
+
+  trainings = Training.where("date >= ?", Date.today)
+                      .order(:date)
+                      .limit(3)
+                      .select("id, name, date, 'Training' as type")
+
+  @upcoming_activities = (races + events + trainings)
+    .sort_by(&:date)
+    .first(6)
   end
 end
