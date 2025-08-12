1de687c7a84008fa9bf2388600ea1153a1bc7869
benjamin grassiano <grassiano.b@gmail.com>
2025-08-12 12:43:29 +0200

    Création des controllers dans le namespace public pour les actions index et show + création des routes + front sur la homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    # Permet d'afficher uniquement les courses à venir
-    @races = Race.where("date >= ?", Date.today).order(date: :asc)
+    # Permet d'afficher les événements à venir
+    @races = Race.where("date >= ?", Date.today).order(date: :asc).limit(2)
+    @trainings = Training.where("date >= ?", Date.today).order(date: :asc).limit(2)
+    @events = Event.where("date >= ?", Date.today).order(date: :asc).limit(2)
   end
 end
