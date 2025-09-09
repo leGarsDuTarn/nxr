bd1795914cc66f58df73e1be83ac644128295aaf
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-09 11:21:31 +0200

    Travail du front du dashboard admin en cours - creeation de card et amelioration du style via scss

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class DashboardController < BaseController
     def index
-      @events = Event.all.order(date: :asc).limit(2)
+      @events = Event.all.order(date: :asc).limit(3)
       @trainings = Training.all.order(date: :asc).limit(4)
       @races = Race.all.order(date: :asc).limit(5)
       @articles = Article.all.order(date: :asc).limit(3)
