a9985c1122e39c4c5323bec3e5649c6d42a8587f
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-16 00:19:28 +0200

    roolback en db suite a installation de trix et retour au dernier commit

--- db/schema.rb
+++ db/schema.rb
@@ -44,11 +44,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_12_075944) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "content"
     t.date "date"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
