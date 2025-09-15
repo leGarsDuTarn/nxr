bd2fb44b48551e30c5da56305e6581dc2b16b29c
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-15 11:09:27 +0200

    creation d'un helper pour gerer l'affichage de la banner correctement peu importe l'event et creation partial pour pouvoir garder le me style sur event, training et race tout en etant DRY

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,34 +1,13 @@
 module ApplicationHelper
-  # Retourne l'URL de l'image à afficher en bannière pour un événement.
+  # Retourne l'URL de l'image à afficher en bannière pour n’importe quel "registerable" (training, race, event)
   # Si une image est attachée via ActiveStorage avec Cloudinary, utilise cette image.
-  # Sinon, retourne une image par défaut hébergée sur Pixabay.
-  def banner_image_url(event)
-    if event.image.attached?
-      url_for(event.image)
+  # Sinon, retourne une image par défaut
+  def banner_image_url(registerable)
+    if registerable.respond_to?(:image) && registerable.image.attached?
+      url_for(registerable.image)
     else
-      "https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645_1280.jpg"
-    end
-  end
-
-  # Retourne l'URL de l'image à afficher en bannière pour un entraînement.
-  # Si une image est attachée via ActiveStorage avec Cloudinary, utilise cette image.
-  # Sinon, retourne une image par défaut hébergée sur Pixabay.
-  def banner_image_training_url(training)
-    if training.image.attached?
-      url_for(training.image)
-    else
-      "https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645_1280.jpg"
-    end
-  end
-
-  # Retourne l'URL de l'image à afficher en bannière pour une course.
-  # Si une image est attachée via ActiveStorage avec Cloudinary, utilise cette image.
-  # Sinon, retourne une image par défaut hébergée sur Pixabay.
-  def banner_image_race_url(race)
-    if race.image.attached?
-      url_for(race.image)
-    else
-      "https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645_1280.jpg"
+      # fallback générique si aucune image n’est attachée
+      asset_path("banner_mcnc.jpeg")
     end
   end
 end
