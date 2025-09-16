17b85fe4f88137c178df620a23c794b76309068e
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-16 11:03:46 +0200

    Amelioration de la taille des image dans les cards dans index race trainings et events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,27 @@ module ApplicationHelper
       asset_path("banner_mcnc.jpeg")
     end
   end
+
+  # Retourne l'image par défaut selon le type de registerable (Race, Training, Event)
+  def default_registerable_image(item)
+    case item.class.name
+    when "Race"
+      "race_flag.png"
+    when "Training"
+      "training_img.png"
+    when "Event"
+      "logo_mcnc_3.png"
+    else
+      "banner_mcnc.jpeg"
+    end
+  end
+
+  # Retourne l’URL d’image (attachée ou fallback)
+  def registerable_image_url(item)
+    if item.respond_to?(:image) && item.image.attached?
+      url_for(item.image)
+    else
+      asset_path(default_registerable_image(item))
+    end
+  end
 end
