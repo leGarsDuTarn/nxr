f8b88c1712793aeb96f8857f98cf683215d8fa7d
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-19 11:27:13 +0200

    Show et index dans le namespace public ok = pagination + recherche par date ou titre

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -1,7 +1,17 @@
 module Public
   class ArticlesController < BaseController
+    include Paginable
     def index
       @articles = Article.all.order(created_at: :desc) # :desc -> + récent en premier
+
+      # Recherche
+      @articles = @articles.search(params[:q]) if params[:q].present?
+
+      # Préchargement ActiveStorage pour éviter les N+1
+      @articles = @articles.with_attached_image if Article.reflect_on_attachment(:image)
+
+      # Pagination Kaminari via concern
+      @articles = apply_pagination(@articles)
     end
 
     def show
