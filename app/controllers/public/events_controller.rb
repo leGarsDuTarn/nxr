1233f9d50d547c98e5e3b87d22dc16695f3b6d56
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-22 10:12:21 +0200

    Index + sow de event via hp et navbar done

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -1,7 +1,17 @@
 module Public
   class EventsController < BaseController
+    include Paginable
     def index
       @events = Event.all.order(created_at: :desc) # :desc -> + récent en premier
+
+      # Recherche (si param q présent)
+      @events = @events.search(params[:q]) if params[:q].present?
+
+      # Préchargement ActiveStorage pour éviter les N+1
+      @events = @events.with_attached_image if Event.reflect_on_attachment(:image)
+
+      # Pagination Kaminari
+      @events = apply_pagination(@events)
     end
 
     def show
