eb6f56d3e53ab7159afefa3ae9d6440bee3fe254
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-25 10:14:12 +0200

    amelioration de la logique metier sur les deux models

--- app/models/legal_notice.rb
+++ app/models/legal_notice.rb
@@ -1,4 +1,13 @@
 class LegalNotice < ApplicationRecord
-  validates :title, presence: true
-  validates :body,  presence: true
+  before_validation :strip_title
+
+  validates :title, presence: { message: "Vous devez renseigner un titre" }, length: { minimum: 3 }
+  validates :body,  presence: { message: "Vous devez renseigner ce champs" }
+
+  private
+
+  # Permet d'enlever les espaces blanc au début et à la fin du titre
+  def strip_title
+    self.title = title&.strip
+  end
 end
