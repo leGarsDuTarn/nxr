8d9dd34f678a4eb15b090f3d62408a94ef4b3b92
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-25 11:12:42 +0200

    Modif du controlleur dashboard admin pour pouvoir afficher les privacy_policie et legal_notice

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -9,6 +9,8 @@ module Admin
       @users = User.all.order(created_at: :asc).limit(3)
       @recent_registrations = Registration.includes(:user, :registerable).order(created_at: :desc).limit(3)
       @clubs = Club.first
+      @privacy_policies = PrivacyPolicy.first
+      @legal_notices = LegalNotice.first
     end
   end
 end
