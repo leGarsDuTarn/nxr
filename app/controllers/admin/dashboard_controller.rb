ff311cb9a0128686d0293427b998d08cb74ad181
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-26 19:43:11 +0200

    modif des limite de card dans les dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,10 +4,10 @@ module Admin
       @events = Event.all.order(date: :asc).limit(3)
       @trainings = Training.all.order(date: :asc).limit(3)
       @races = Race.all.order(date: :asc).limit(3)
-      @articles = Article.all.order(date: :asc).limit(3)
-      @galleries = Gallery.all.order(date: :asc).limit(3)
-      @users = User.all.order(created_at: :asc).limit(3)
-      @recent_registrations = Registration.includes(:user, :registerable).order(created_at: :desc).limit(3)
+      @articles = Article.all.order(date: :asc).limit(6)
+      @galleries = Gallery.all.order(date: :asc).limit(6)
+      @users = User.all.order(created_at: :asc).limit(6)
+      @recent_registrations = Registration.includes(:user, :registerable).order(created_at: :desc).limit(6)
       @club = Club.first
       @privacy_policy = PrivacyPolicy.first
       @legal_notice = LegalNotice.first
--- app/controllers/members/dashboard_controller.rb
+++ app/controllers/members/dashboard_controller.rb
@@ -2,9 +2,9 @@ module Members
   class DashboardController < BaseController
     def index
       @registrations = current_user.registrations.includes(:registerable)
-      @events = Event.all.order(date: :asc).limit(10)
-      @trainings = Training.all.order(date: :asc).limit(10)
-      @races = Race.all.order(date: :asc).limit(10)
+      @events = Event.all.order(date: :asc).limit(6)
+      @trainings = Training.all.order(date: :asc).limit(6)
+      @races = Race.all.order(date: :asc).limit(6)
     end
   end
 end
