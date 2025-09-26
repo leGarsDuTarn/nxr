7bee022a3510f5199116debe66626b793687e62b
Benjamin Grassiano <grassiano.b@gmail.com>
2025-09-26 11:04:53 +0200

    Modification style doc legal

--- app/controllers/public/legal_notice_controller.rb
+++ app/controllers/public/legal_notice_controller.rb
@@ -1,6 +1,10 @@
 module Public
   class LegalNoticeController < BaseController
     def show
+      # Ici comme l'admin doit creer une politique de confidentialité
+      # si l'admin n'a pas déjà crée le doc alors 'nil' et heroku casse
+      # donc .first_or_initialize' évite le nil, renvoie le premier enregistrement s'il existe, ou en instancie un nouveau.
+      # et Heroku ne casse plus
       @legal_notice = LegalNotice.first_or_initialize
     end
   end
--- app/controllers/public/privacy_policy_controller.rb
+++ app/controllers/public/privacy_policy_controller.rb
@@ -1,6 +1,10 @@
 module Public
   class PrivacyPolicyController < BaseController
     def show
+      # Ici comme l'admin doit creer une politique de confidentialité
+      # si l'admin n'a pas déjà crée le doc alors 'nil' et heroku casse
+      # donc .first_or_initialize' évite le nil, renvoie le premier enregistrement s'il existe, ou en instancie un nouveau.
+      # et Heroku ne casse plus
       @privacy_policy = PrivacyPolicy.first_or_initialize
     end
   end
